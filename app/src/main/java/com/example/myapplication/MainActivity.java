98a6fa26dba896429b0359b71e150febe06864d0
sanahid <sanstudio120@gmail.com>
2024-09-05 20:50:33 +0600

    commited

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+private static int ImageIndex = 0;
+    private static final int[] imageResources = {R.drawable.img, R.drawable.img_1,R.drawable.img_2,R.drawable.img_3,R.drawable.img_4};
+    private ImageView imageView;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        imageView = findViewById(R.id.iv_imageView);
+        button = findViewById(R.id.bt_change_image);
+        button.setOnClickListener(v -> {
+            ImageIndex++;
+            if (ImageIndex >= imageResources.length) {
+                ImageIndex = 0;
+            }
+            imageView.setImageResource(imageResources[ImageIndex]);
+            Toast.makeText(this, "Image changed!!!", Toast.LENGTH_SHORT).show();
         });
     }
 }
